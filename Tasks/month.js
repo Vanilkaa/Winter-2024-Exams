f08c0ed7df4631f1dccd5ca13a6c04e68ac50bbc
Vanilkaa <132566705+Vanilkaa@users.noreply.github.com>
2024-01-14 20:11:47 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,15 +1,14 @@
 // Get month number
 
-//Step 3
-// Change identifiers case
+//Step 4
+// Remove useless variable l
 
 'use strict';
 
 let months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec',];
 
 const month = (s) => {
-  let l = months.length;
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < months.length; i++) {
     if (s.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
