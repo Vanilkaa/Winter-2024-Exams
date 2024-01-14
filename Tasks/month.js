51213beb23173643872c808f6dd43991dfd75b45
Vanilkaa <132566705+Vanilkaa@users.noreply.github.com>
2024-01-14 18:44:45 +0200

    Update month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,11 +1,14 @@
 // Get month number
 
-//Step 1
-// Format code indentation and spacing
+//Step 2
+// Add 'use strict';
+// Add `const`
 
-Months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec',];
+'use strict';
 
-Month = (s) => {
+let Months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec',];
+
+const Month = (s) => {
   l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
