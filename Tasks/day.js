052e04688e2cc1d68ba6c6d08272e1c5b912ef16
Vanilkaa <132566705+Vanilkaa@users.noreply.github.com>
2024-01-14 12:21:50 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,7 +1,7 @@
 // Get day number
 
-// Step 3
-// Move variable declaration to cycle
+// Step 4
+// Shorten if statement to one line
 
 'use strict';
 
@@ -9,9 +9,7 @@ let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
   for (let i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
+    if (s.startsWith(D[i].toLowerCase())) return i + 1;
   }
   return -1;
 };
